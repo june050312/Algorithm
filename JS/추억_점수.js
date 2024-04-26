86d6f203864561ce4dbb6e92a2deb31ec7cc2188
june050312 <june050312@gmail.com>
2024-04-26 22:23:02 +0900

    추억 점수 수정

--- JS/추억_점수.js
+++ JS/추억_점수.js
@@ -1,6 +1,6 @@
-name = ["may", "kein", "kain", "radi"]
-yearning = [5, 10, 1, 3]
-photo = [["may", "kein", "kain", "radi"],["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]]
+let name = ["may", "kein", "kain", "radi"]
+let yearning = [5, 10, 1, 3]
+let photo = [["may", "kein", "kain", "radi"],["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]]
 
 let missVal = new Object()
 let answer = new Array(photo.length).fill(0)
