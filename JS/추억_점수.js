295633fd6a4be56dc86254d8c4f07e632e1ea9df
june050312 <june050312@gmail.com>
2024-04-26 22:06:21 +0900

    추억 점수

---
+++ JS/추억_점수.js
@@ -0,0 +1,18 @@
+name = ["may", "kein", "kain", "radi"]
+yearning = [5, 10, 1, 3]
+photo = [["may", "kein", "kain", "radi"],["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]]
+
+let missVal = new Object()
+let answer = new Array(photo.length).fill(0)
+
+for (let i=0;i<name.length;i++) {
+    missVal[name[i]] = yearning[i]
+}
+
+for (let i=0;i<photo.length;i++) {
+    photo[i].forEach(person => {
+        missVal[person] != undefined ? answer[i] += missVal[person] : answer[i]
+    });
+}
+
+console.log(answer);
