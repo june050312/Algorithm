204c04b66de029acf29e53015df2e1ad13b58bae
june050312 <june050312@gmail.com>
2024-04-29 10:53:43 +0900

    요격 시스템 블로그 보고 다시 풀이

--- JS/요격_시스템.js
+++ JS/요격_시스템.js
@@ -1,43 +1,23 @@
 let targets = [[4,5],[4,8],[10,14],[11,13],[5,12],[3,7],[1,4]]
 
-let fieldLength = 0
-targets.forEach(target => {
-    Math.max(...target) > fieldLength ? fieldLength = Math.max(...target) : fieldLength
-});
-
-let field = new Array(fieldLength).fill(0)
-
-targets.forEach(target => {
-    for (let i=target[0]-1;i<target[1];i++) {
-        field[i]++
-    }
-});
-
 let cnt = 0
-while(1) {
-    let flag = 0
-    field.forEach(element => {
-        flag += element
-    });
-    if (flag == 0) {
-        break
+
+targets.sort((m1, m2) => {
+    if (m1[1] < m2[1]) return -1
+    if (m1[1] > m2[1]) return 1
+    if (m1[1] === m2[1]) {
+        if (m1[0] < m2[0]) return -1
+        if (m1[0] > m2[0]) return 1
+        if (m1[0] === m2[0]) return 0
     }
+})
 
-    let removes = []
-    let pos = field.indexOf(Math.max(...field)) + 1.5
-
-    targets.forEach(target => {
-        if (target[0] < pos && pos < target[1]) {
-            removes.push(target)
-        }
-    });
-
-    removes.forEach(remove => {
-        for (let i=remove[0]-1;i<remove[1];i++) {
-            field[i]--
-        }
-    });
-    cnt++
-}
+let e = 0
+targets.forEach(target => {
+    if (target[0] >= e) {
+        cnt++
+        e = target[1] 
+    }
+});
 
 console.log(cnt);
