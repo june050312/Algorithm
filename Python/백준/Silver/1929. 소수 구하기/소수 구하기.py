901b49119c1fab13c59658ea023b5f9b48f0246c
june050312 <81077519+june050312@users.noreply.github.com>
2024-12-27 16:04:14 +0900

    [Silver III] Title: 소수 구하기, Time: 472 ms, Memory: 48400 KB -BaekjoonHub

---
+++ Python/백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,20 @@
+import sys
+
+check = [0] * 1000000
+check[0] = 1
+check[1] = 1
+
+primeList = []
+
+for i in range(2, 1000000):
+    if check[i] == 0:
+        primeList.append(i)
+        for j in range(2*i, 1000000, i):
+            check[j] = 1
+
+cnt = 0
+start, end = map(int, sys.stdin.readline().strip().split())
+for i in check[start : end + 1]:
+    if i == 0:
+        print(cnt + start)
+    cnt += 1
