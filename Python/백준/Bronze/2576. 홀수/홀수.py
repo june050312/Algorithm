af70ea4dd8b56c3868b53c795edab028c598bdb3
june050312 <81077519+june050312@users.noreply.github.com>
2025-01-18 02:00:05 +0900

    [Bronze III] Title: 홀수, Time: 32 ms, Memory: 32544 KB -BaekjoonHub

---
+++ Python/백준/Bronze/2576. 홀수/홀수.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+odd = []
+for _ in range(7):
+    num = int(input())
+    if num % 2 != 0:
+        odd.append(num)
+
+if odd:
+    print(sum(odd))
+    print(min(odd))
+else:
+    print(-1)
