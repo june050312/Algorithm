a90d5b1ec41ae3423a85aa9d2543935bd4e5e7bb
june050312 <81077519+june050312@users.noreply.github.com>
2025-01-17 16:31:16 +0900

    [Bronze I] Title: 색종이, Time: 224 ms, Memory: 39900 KB, Score: 11 point -BaekjoonHub

---
+++ Python/백준/Bronze/10163. 색종이/색종이.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+board = [[0 for _ in range(1001)] for _ in range(1001)]
+n = int(input())
+for _ in range(n):
+    x, y, w, h = map(int, input().split())
+    for i in range(h):
+        for j in range(w):
+            board[y + i][x + j] += 1
+
+for i in range(n):
+    cnt = 0
+    for j in board:
+        cnt += j.count(i + 1)
+    print(cnt)
